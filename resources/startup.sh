c1026f3251bc1b2bd5c2553ff7b5c8798298f04e
Niklas <niklas.hinze@cloudogu.com>
2022-06-01 11:02:59 +0200

    Update warp menu to 1.5.0. Rename ingress-nginx to nginx-ingress #2

--- resources/startup.sh
+++ resources/startup.sh
@@ -22,11 +22,11 @@ sed -i "s|/warp/menu.json|/warp/menu/menu.json|g" /var/www/html/warp/warp.js
 # Start nginx
 echo "[nginx] starting nginx service..."
 /nginx-ingress-controller \
-  --publish-service="${POD_NAMESPACE}"/ingress-nginx-controller \
+  --publish-service="${POD_NAMESPACE}"/nginx-ingress-controller \
   --election-id=ingress-controller-leader \
-  --controller-class=k8s.io/ingress-nginx \
+  --controller-class=k8s.io/nginx-ingress \
   --ingress-class=k8s-ecosystem-ces-service \
-  --configmap="${POD_NAMESPACE}"/ingress-nginx-controller \
+  --configmap="${POD_NAMESPACE}"/nginx-ingress-controller \
   --validating-webhook=:8443 \
   --validating-webhook-certificate=/usr/local/certificates/cert \
   --validating-webhook-key=/usr/local/certificates/key \
